9d124b241e361897f61f9a1644884e4359b2ac7b
Davide Principi <davide.principi@nethesis.it>
2013-02-04 16:43:28 +0100

    Path renaming and cleaning up

--- root/usr/share/nethserver/sogo-frontends/updates.php
+++ root/usr/share/nethserver/sogo-frontends/updates.php
@@ -1,5 +1,29 @@
 <?php
-/* Federico Simoncelli <federico@nethesis.it> */
+/* 
+ *  Copyright (C) 2006-2012 Inverse inc.
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *         Francis Lachapelle <flachapelle@inverse.ca>
+ *
+ *  Copyright (C) 2013 Nethesis srl
+ *
+ * Author: Federico Simoncelli <federico@nethesis.it>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 $pluginid = @$_REQUEST['plugin'];
 $platform = @$_REQUEST['platform'];
